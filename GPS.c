56edc15b343fee6959a8120d228af5d07af64a7e
Nima Askari ----- نیما عسکری <nimaltd@yahoo.com>
2018-07-01 12:54:32 +0430

    Add files via upload

---
+++ GPS.c
@@ -0,0 +1,72 @@
+#include "GPSConfig.h"
+#include "GPS.h"
+#include "usart.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+GPS_t GPS;
+//##################################################################################################################
+double convertDegMinToDecDeg (float degMin)
+{
+  double min = 0.0;
+  double decDeg = 0.0;
+ 
+  //get the minutes, fmod() requires double
+  min = fmod((double)degMin, 100.0);
+ 
+  //rebuild coordinates in decimal degrees
+  degMin = (int) ( degMin / 100 );
+  decDeg = degMin + ( min / 60 );
+ 
+  return decDeg;
+}
+//##################################################################################################################
+void	GPS_Init(void)
+{
+	GPS.rxIndex=0;
+	HAL_UART_Receive_IT(&_GPS_USART,&GPS.rxTmp,1);	
+}
+//##################################################################################################################
+void	GPS_CallBack(void)
+{
+	GPS.LastTime=HAL_GetTick();
+	if(GPS.rxIndex < sizeof(GPS.rxBuffer)-2)
+	{
+		GPS.rxBuffer[GPS.rxIndex] = GPS.rxTmp;
+		GPS.rxIndex++;
+	}	
+	HAL_UART_Receive_IT(&_GPS_USART,&GPS.rxTmp,1);
+}
+//##################################################################################################################
+void	GPS_Process(void)
+{
+	if( (HAL_GetTick()-GPS.LastTime>50) && (GPS.rxIndex>0))
+	{
+		char	*str;
+		#if (_GPS_DEBUG==1)
+		printf("%s",GPS.rxBuffer);
+		#endif
+		str=strstr((char*)GPS.rxBuffer,"$GPGGA,");
+		if(str!=NULL)
+		{
+			memset(&GPS.GPGGA,0,sizeof(GPS.GPGGA));
+			sscanf(str,"$GPGGA,%2hhd%2hhd%2hhd.%3hd,%f,%c,%f,%c,%hhd,%hhd,%f,%f,%c,%hd,%s,*%2s\r\n",&GPS.GPGGA.UTC_Hour,&GPS.GPGGA.UTC_Min,&GPS.GPGGA.UTC_Sec,&GPS.GPGGA.UTC_MicroSec,&GPS.GPGGA.Latitude,&GPS.GPGGA.NS_Indicator,&GPS.GPGGA.Longitude,&GPS.GPGGA.EW_Indicator,&GPS.GPGGA.PositionFixIndicator,&GPS.GPGGA.SatellitesUsed,&GPS.GPGGA.HDOP,&GPS.GPGGA.MSL_Altitude,&GPS.GPGGA.MSL_Units,&GPS.GPGGA.AgeofDiffCorr,GPS.GPGGA.DiffRefStationID,GPS.GPGGA.CheckSum);
+			if(GPS.GPGGA.NS_Indicator==0)
+				GPS.GPGGA.NS_Indicator='-';
+			if(GPS.GPGGA.EW_Indicator==0)
+				GPS.GPGGA.EW_Indicator='-';
+			if(GPS.GPGGA.Geoid_Units==0)
+				GPS.GPGGA.Geoid_Units='-';
+			if(GPS.GPGGA.MSL_Units==0)
+				GPS.GPGGA.MSL_Units='-';
+			GPS.GPGGA.LatitudeDecimal=convertDegMinToDecDeg(GPS.GPGGA.Latitude);
+			GPS.GPGGA.LongitudeDecimal=convertDegMinToDecDeg(GPS.GPGGA.Longitude);			
+		}		
+		memset(GPS.rxBuffer,0,sizeof(GPS.rxBuffer));
+		GPS.rxIndex=0;
+	}
+	HAL_UART_Receive_IT(&_GPS_USART,&GPS.rxTmp,1);
+}
+//##################################################################################################################
---
+++ GPSConfig.h
@@ -0,0 +1,9 @@
+#ifndef _GPSCONFIG_H_
+#define _GPSCONFIG_H_
+
+#define	_GPS_USART					huart3
+#define	_GPS_DEBUG					0
+
+
+
+#endif
